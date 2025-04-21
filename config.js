93000fcdf3e20ee0c0b1b37a61b7bfd65e8ff0cd
SEƗSĦƗRO⚔ṈȺGƗ.il <seishironagi.rukasproyect@gmail.com>
2025-04-20 19:40:50 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -10,25 +10,25 @@ import moment from 'moment-timezone'
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 // Staff De CrowBot
 global.owner = [
-   ['50557865603', 'Creador WillZek💛', true],
-   ['5212731595017', 'CrowBot Soporte', true],
-   ['584148256527', 'Dev Diomar', true],
-   ['584120346669', 'Dev ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜', true],
-   ['584241836217', 'Dev Prak', true],
-   ['51968382008', 'Dev Darkcore', true],
-   ['5216633900512','Dev ʙʀᴀʏᴀɴᴏғᴄ', true],
+   ['50768961657', 'dueño Sxnti7 Legi💛', true],
+   ['51920227615', 'ᴬⁿᵍᵉˡⁱᵗʰᵒ ᵒᶠⁱᶜⁱᵃˡ', true],
+   ['', 'nadie x3', true],
+   ['', 'nadie x2', true],
+   ['', 'que nadie dije', true],
+   ['', 'nadie tampoco :v', true],
+   ['','nadie :v', true],
 ]
 
 global.creadorbot = [
-   ['50557865603', 'WillZek', true],
+   ['50768961657', 'Sxnti7 Legi', true],
 ]
   
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.mods = ['50368471855', '51920227615', '5219831715910', '573007796996', '50488198573', '51991055096', '5212731590195', '50557865603', '595975677765', '584245610338']
+global.mods = ['']
 
-global.prems = ['573215751237']
+global.prems = ['']
 
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
@@ -36,26 +36,26 @@ global.prems = ['573215751237']
 global.libreria = 'Baileys'
 global.baileys = 'V 6.7.16' 
 global.vs = '2.2.0'
-global.nameqr = 'CrowBot-MF'
-global.namebot = 'CrowBot-MF'
+global.nameqr = '𝐆𝐚𝐧𝐠 𝟑𝟑𝟑 𝐛𝐨𝐭'
+global.namebot = '𝐆𝐚𝐧𝐠 𝟑𝟑𝟑 𝐛𝐨𝐭'
 global.sessions = 'CrowSession'
 global.jadi = 'CrowJadiBot' 
 global.yukiJadibts = true // Activado
 global.ch = {
-ch1: '120363387375075395@newsletter',
-ch2: '120363368073378190@newsletter'
+ch1: '120363419109208930@newsletter',
+ch2: '120363401254649002@newsletter'
 }
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.packname = '【★】𝐒𝐭𝐚𝐫𝐕𝐨𝐢𝐝-𝐂𝐥𝐮𝐛 ❖'
-global.botname = '(✦◠‿◠)𝑪𝒓𝒐𝒘𝑩𝒐𝒕-𝑺𝑻'
-global.wm = '⏤͟͟͞͞⋆⬪࣪ꥈ🍨★ ׄ ꒱ 𝑪𝒓𝒐𝒘𝑩𝒐𝒕 - 𝑺𝑻 ୭'
-global.author = '(⁎˃ᴗ˂⁎)𝐌𝐚𝐝𝐞 𝐁𝐲 𝐒𝐭𝐚𝐫𝐕𝐨𝐢𝐝-𝐂𝐥𝐮𝐛𓆪'
-global.dev = '© 𝖯᥆𝗐ᥱ𝗋ᥱძ ᑲᥡ 𝖲ᥙᥒ𝖿ᥣᥲ𝗋ᥱ  ☂︎  𝖳ᥱᥲ𝗆'
+global.packname = '𝐆𝐚𝐧𝐠 𝟑𝟑𝟑 𝐛𝐨𝐭 ❖'
+global.botname = '𝐆𝐚𝐧𝐠 𝟑𝟑𝟑 𝐛𝐨𝐭'
+global.wm = '⏤͟͟͞͞⋆⬪࣪ꥈ🍨★ ׄ ꒱ 𝐆𝐚𝐧𝐠 𝟑𝟑𝟑 𝐛𝐨𝐭'
+global.author = '(⁎˃ᴗ˂⁎)𝐌𝐚𝐝𝐞 𝐁𝐲 Sxnti7 Legi'
+global.dev = '© 𝖯᥆𝗐ᥱ𝗋ᥱძ ᑲᥡ Sxnti7 Legi'
 global.espera = '✰ 𝐄𝐬𝐩𝐞𝐫𝐚 𝐔𝐧 𝐌𝐨𝐦𝐞𝐧𝐭𝐨, 𝐄𝐬𝐭𝐚𝐦𝐨𝐬 𝐄𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐒𝐮 𝐏𝐞𝐝𝐢𝐝𝐨 (✿◠‿◠)'
-global.namebot = 'ᥴr᥆ᥕᑲ᥆𝗍 s𝗍'
-global.textbot = `「 🔱 𝐂𝐫𝐨𝐰𝐁𝐨𝐭 - 𝐒𝐓 🔱 」`
+global.namebot = '𝐆𝐚𝐧𝐠 𝟑𝟑𝟑 𝐛𝐨𝐭'
+global.textbot = `「 🔱 𝐆𝐚𝐧𝐠 𝟑𝟑𝟑 𝐛𝐨𝐭 🔱 」`
 global.vs = '2.2.0'
 global.publi = '✰𝐒𝐢𝐠𝐮𝐞 𝐄𝐥 𝐂𝐚𝐧𝐚𝐥👇'
 
@@ -69,16 +69,16 @@ global.adios = fs.readFileSync('./media/adios.jpg')
 global.catalogo = fs.readFileSync('./media/catalogo.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-global.repobot = 'https://github.com/WillZek/CrowBot-ST'
+global.repobot = 'https://github.com/willzek/CrowBot-ST'
 global.grupo = 'https://chat.whatsapp.com/B7POPrlJiZf7UWHJKvzVkx'
 global.gteam = 'https://chat.whatsapp.com/EpkR7XS0hrMDYk89yw57HZ'
 global.gsupport = 'https://chat.whatsapp.com/K5GLB5CXp3iAl4XsDzav1W'
 global.comu = 'https://chat.whatsapp.com/Bgfb6Ag13BDCvwzOtldlCm'
-global.channel = 'https://whatsapp.com/channel/0029Vb1AFK6HbFV9kaB3b13W'
+global.channel = 'https://whatsapp.com/channel/0029Vb5g2DJ8F2pIV0CuB53V'
 global.insta = 'https://www.instagram.com/crowbot-wa'
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '👑【✫𝚃𝙴𝙰𝙼  乂 𝚂𝚃𝙰𝚁𝙲𝙾𝚁𝙴✫】🎩', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "543876577197-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '👑【✫༆ᴺᵉˣᵘˢ乂club숬✫】🎩', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
