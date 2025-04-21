b01806cd84e22ac7dc6973e64080e8b2c23bb50c
SEƗSĦƗRO⚔ṈȺGƗ.il <seishironagi.rukasproyect@gmail.com>
2025-04-20 21:05:40 -0500

    Actualizar main-menusearch.js

--- plugins/main-menusearch.js
+++ plugins/main-menusearch.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'buscador': 'Busquedas',
 }
-// let img = 'https://files.catbox.moe/w3s79j.jpg'
+// let img = 'https://files.catbox.moe/sc1yza.jpg'
 let img = './media/menus/Menu.jpg';
 
   try {
